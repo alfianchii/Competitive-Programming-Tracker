69ab0d3a03ece286db3394e0ccf2591978be47b7
Alfian Taka <pianaee379@gmail.com>
2026-03-24 01:23:32 +0700

    feat: UVa 10071 [AC]

--- chapter-1/1-entry-point/io-and-sequences-only/2-uva-10071-back-to-highschool-physics.cpp
+++ chapter-1/1-entry-point/io-and-sequences-only/2-uva-10071-back-to-highschool-physics.cpp
@@ -1,20 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-// Ini Taka
 int main() {
   freopen("2-uva-10071.in", "r", stdin);
   freopen("2-uva-10071.out", "w", stdout);
 
-  while (1) {
-    int v, ans = 1;
-    char dummy;
-
-    while (scanf("%d%c", &v, &dummy) != EOF) {
-      ans *= v;
-      if (dummy == '\n') break;
-    }
-
-    printf("%d\n", ans * 2);
-    if (feof(stdin)) break;
+  int v, t;
+  while (scanf("%d %d", &v, &t) != EOF) {
+    printf("%d\n", v * t * 2);
   }
 }
