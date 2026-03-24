db8d9392decdfa0518e32d8c1c04d0999ed94f0b
Alfian Taka <pianaee379@gmail.com>
2026-03-24 23:41:30 +0700

    chore: remove unused freopen

--- chapter-1/1-entry-point/io-and-sequences-only/5-kattis-carrots.cpp
+++ chapter-1/1-entry-point/io-and-sequences-only/5-kattis-carrots.cpp
@@ -3,8 +3,6 @@
 using namespace std;
 
 int main() {
-  freopen("5-kattis-carrots.in", "r", stdin);
-  freopen("5-kattis-carrots.out", "w", stdout);
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
