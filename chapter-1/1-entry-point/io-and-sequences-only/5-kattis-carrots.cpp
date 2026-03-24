0d1b11e080025530c5e0529b2ba767061b488e14
Alfian Taka <pianaee379@gmail.com>
2026-03-24 22:35:11 +0700

    feat: Kattis - carrots

---
+++ chapter-1/1-entry-point/io-and-sequences-only/5-kattis-carrots.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  freopen("5-kattis-carrots.in", "r", stdin);
+  freopen("5-kattis-carrots.out", "w", stdout);
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int c, p;
+  cin >> c;
+  cin >> p;
+
+  cout << p;
+}
