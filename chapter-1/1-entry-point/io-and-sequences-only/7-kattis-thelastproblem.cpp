693c1d1f8c94512b530ae93aab5fce332e7726f2
Alfian Taka <pianaee379@gmail.com>
2026-03-25 20:41:04 +0700

    refactor: Kattis - thelastproblem [AC] with scanf and regex

--- chapter-1/1-entry-point/io-and-sequences-only/7-kattis-thelastproblem.cpp
+++ chapter-1/1-entry-point/io-and-sequences-only/7-kattis-thelastproblem.cpp
@@ -3,12 +3,7 @@
 using namespace std;
 
 int main() {
-  char s;
-  string name;
-
-  while (scanf("%c", &s) != EOF) {
-    if (s != '\n') name += s;
-  }
-
-  cout << "Thank you, " << name << ", and farewell!";
+  char name[51];
+  scanf("%[^\n]", name);
+  printf("Thank you, %s, and farewell!", name);
 }
