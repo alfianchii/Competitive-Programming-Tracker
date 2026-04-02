b047b2738c563e2ede16b2b072d30a7879503442
Alfian Taka <pianaee379@gmail.com>
2026-04-02 15:58:28 +0700

    feat: Kattis - acm [AC]

---
+++ chapter-1/1-entry-point/1d-array-manipulation/5-kattis-acm.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int time; char letter; string verdict; 
+  vector<int> wrongAnswers(100, 0);
+  int ac = 0, score = 0;
+
+  cin >> time >> letter >> verdict;
+
+  while (time != -1) {
+    if (verdict == "right") {
+      score += time; ac++;
+
+      if (wrongAnswers[letter - 'A'] > 0) {
+        score += 20 * wrongAnswers[letter - 'A'];
+        wrongAnswers[letter - 'A'] = 0;
+      }
+    } else wrongAnswers[letter - 'A']++;
+
+    cin >> time >> letter >> verdict;
+  }
+
+  cout << ac << " " << score;
+}
