df199f8ebdeb2facee7d17baae3fde0702143a03
Alfian Taka <pianaee379@gmail.com>
2026-04-12 14:37:56 +0700

    feat: modified basic string 4 w/ hash map

--- chapter-1/exercises/basic-string-4.cpp
+++ chapter-1/exercises/basic-string-4.cpp
@@ -16,9 +16,18 @@ int main() {
 
   sort(tokens.begin(), tokens.end());
 
-  for (string token : tokens) {
-    cout << token << "\n";
+  for (string token : tokens) cout << token << "\n";
+
+  unordered_map<string, int> freq;
+  for (string token : tokens) freq[token]++;
+  string ans; int maxFreq = 0;
+  for (auto &fr : freq) {
+    if (fr.second > maxFreq) {
+      maxFreq = fr.second;
+      ans = fr.first;
+    }
   }
 
   cout << "Smallest: " << tokens[0] << "\n";
+  cout << "Most frequent: " << ans << " (" << maxFreq << " times)\n";
 }
