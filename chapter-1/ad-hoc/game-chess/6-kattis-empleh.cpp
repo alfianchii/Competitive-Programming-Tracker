e00c90d5ead781b222f13bad02602a27e3c4d437
Alfian Taka <pianaee379@gmail.com>
2026-04-24 15:47:42 +0700

    refactor: Kattis - empleh

--- chapter-1/ad-hoc/game-chess/6-kattis-empleh.cpp
+++ chapter-1/ad-hoc/game-chess/6-kattis-empleh.cpp
@@ -12,14 +12,6 @@ void parse(const string &s, unordered_map<string, char> &mp) {
   }
 }
 
-bool isOdd(int num) {
-  return num % 2 == 1;
-}
-
-bool isEven(int num) {
-  return num % 2 == 0;
-}
-
 int main() {
   unordered_map<string, char> white, black;
   
@@ -32,47 +24,29 @@ int main() {
 
   int boardRow = 8;
   for (int row = 1; row <= 17; row++) {
-    int color;
-    bool isSquare = true;
-
-    if (isOdd(row / 2)) color = 1;
-    else if (isEven(row / 2)) color = 2;
-
-    for (int col = 1; col <= 8; col++) {
-      char colName = 'a' + col - 1;
-
-      if (isOdd(row)) {
-        cout << "+---";
-        if (col >= 8) cout << "+";
-
-        isSquare = false;
-        continue;
-      } else if (isEven(row)) {
+    if (row % 2 == 1) {
+      for (int col = 1; col <= 8; col++) cout << "+---";
+      cout << "+\n";
+    } else {
+      for (int col = 1; col <= 8; col++) {
+        char pawn = 0;
         cout << "|";
-        string square, pawn = ""; pawn += colName; pawn += to_string(boardRow);
-        if (white.count(pawn)) {
-          char piece = white.count(pawn) ? toupper(white[pawn]) : '.';
-          if (isOdd(color)) square = "." + string(1, piece) + ".";
-          else square = ":" + string(1, piece) + ":";
-        } else if (black.count(pawn)) {
-          char piece = black.count(pawn) ? tolower(black[pawn]) : '.';
-          if (isOdd(color)) square = "." + string(1, piece) + ".";
-          else square = ":" + string(1, piece) + ":";
-        } else {
-          if (isOdd(color)) square = "...";
-          else if (isEven(color)) square = ":::";
-        }
 
-        cout << square;
+        char colName = 'a' + col - 1;
+        string pos = string(1, colName) + to_string(boardRow);
 
-        if (col >= 8) cout << "|";
+        if (white.count(pos)) pawn = toupper(white[pos]);
+        else if (black.count(pos)) pawn = tolower(black[pos]);
 
-        color++;
-        continue;
+        bool whiteSquare = (boardRow + col) % 2 == 1;
+        char bg = whiteSquare ? '.' : ':';
+
+        if (pawn) cout << bg << pawn << bg;
+        else cout << string(3, bg);
       }
-    }
 
-    if (isSquare) boardRow--;
-    cout << "\n";
+      cout << "|\n";
+      boardRow--;
+    }
   }
 }
