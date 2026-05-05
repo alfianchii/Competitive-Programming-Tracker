464b353ff7fc7784b6b44dbdbb232a6b498b2659
Alfian Taka <pianaee379@gmail.com>
2026-05-05 15:58:12 +0700

    feat: NeetCode; Stack - Daily Temperatures [AC]

--- Platforms/NeetCode/stack/daily_temperatures/solution.go
+++ Platforms/NeetCode/stack/daily_temperatures/solution.go
@@ -1,5 +1,39 @@
 package NeetCode
 
+type Stack struct {
+	data []int
+}
+
+func (this *Stack) Push(value int) {
+	this.data = append(this.data, value)
+}
+
+func (this *Stack) Pop() {
+	this.data = this.data[:len(this.data)-1]
+}
+
+func (this *Stack) Top() int {
+	return this.data[len(this.data)-1]
+}
+
+func (this *Stack) IsEmpty() bool {
+	return len(this.data) == 0
+}
+
 func DailyTemperatures(temperatures []int) []int {
-	return nil
+	res := make([]int, len(temperatures))
+	stack := Stack{}
+
+	for currIdx, temp := range temperatures {
+		for !stack.IsEmpty() && temp > temperatures[stack.Top()] {
+			prevIdx := stack.Top()
+			stack.Pop()
+
+			res[prevIdx] = currIdx - prevIdx
+		}
+
+		stack.Push(currIdx)
+	}
+
+	return res
 }
