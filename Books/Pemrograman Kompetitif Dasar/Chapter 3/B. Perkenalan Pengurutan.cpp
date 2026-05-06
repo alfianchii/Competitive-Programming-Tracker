09e11a0cc07d92fbf5f215669e4c5a8cfe6a79f9
Alfian Taka <pianaee379@gmail.com>
2026-05-06 10:58:32 +0700

    fix: `B. Perkenalan Pengurutan` w/ bubble sort [TLE]

--- Books/Pemrograman Kompetitif Dasar/Chapter 3/B. Perkenalan Pengurutan.cpp
+++ Books/Pemrograman Kompetitif Dasar/Chapter 3/B. Perkenalan Pengurutan.cpp
@@ -2,9 +2,25 @@
 
 using namespace std;
 
+void bubbleSort(vector<double>& nums) {
+  int n = nums.size() - 1;
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n - i; j++) {
+      if (nums[j] > nums[j + 1]) {
+        double temp = nums[j];
+        nums[j] = nums[j + 1];
+        nums[j + 1] = temp;
+      }
+    }
+  }
+}
+
 int main() {
   int n; cin >> n;
   vector<double> nums(n); for (int i = 0; i < n; i++) cin >> nums[i];
+
+  bubbleSort(nums);
 
   double res = 0;
   int mid = nums.size() / 2;
