11c2436741b9d643d65d089a7112bd6f0a82f7c8
Alfian Taka <pianaee379@gmail.com>
2026-05-07 13:27:55 +0700

    fix: `F. Perkenalan Pencarian Biner II`; the prefix [AC]

--- Books/Pemrograman Kompetitif Dasar/Chapter 3/F. Perkenalan Pencarian Biner II.cpp
+++ Books/Pemrograman Kompetitif Dasar/Chapter 3/F. Perkenalan Pencarian Biner II.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int findAnimalType(int x, vector<int>& types) {
-  int left = 1, right = types.size();
+  int left = 0, right = types.size() - 1;
 
   while (left < right) {
     int mid = (left + right) / 2;
@@ -11,7 +11,7 @@ int findAnimalType(int x, vector<int>& types) {
     else right = mid;
   }
 
-  return left;
+  return ++left;
 }
 
 int main() {
@@ -19,8 +19,8 @@ int main() {
   vector<int> types(n); for (int i = 0; i < n; i++) cin >> types[i];
 
   vector<int> prefix(n);
-  prefix[0] = 0;
-  for (int i = 1; i < n; i++) prefix[i] = prefix[i - 1] + types[i - 1];
+  prefix[0] = types[0];
+  for (int i = 1; i < n; i++) prefix[i] = prefix[i - 1] + types[i];
 
   int q; cin >> q;
   while (q--) {
