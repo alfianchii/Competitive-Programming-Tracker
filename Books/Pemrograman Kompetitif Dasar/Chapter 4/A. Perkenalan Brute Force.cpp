fec80cea3f99129f57d2ce7508f071687df8dd51
Alfian Taka <pianaee379@gmail.com>
2026-05-08 14:12:46 +0700

    fix: `A. Perkenalan Brute Force` [WA]

--- Books/Pemrograman Kompetitif Dasar/Chapter 4/A. Perkenalan Brute Force.cpp
+++ Books/Pemrograman Kompetitif Dasar/Chapter 4/A. Perkenalan Brute Force.cpp
@@ -4,14 +4,18 @@ using namespace std;
 
 int main() {
   string a, b; cin >> a >> b;
-  int miss = 0;
 
-  for (int i = 0; i < a.size(); i++) {
-    if (a[i + miss] != b[i]) {
+  int miss = 0, i = 0, j = 0;
+  while (i < a.size() && j < b.size()) {
+    if (a[i] == b[j]) {
+      i++;
+      j++;
+    } else {
       miss++;
+      i++;
     }
   }
-
-  if (miss == 1) cout << "Tentu saja bisa!\n";
+  
+  if (miss <= 1) cout << "Tentu saja bisa!\n";
   else cout << "Wah, tidak bisa :(\n";
 }
