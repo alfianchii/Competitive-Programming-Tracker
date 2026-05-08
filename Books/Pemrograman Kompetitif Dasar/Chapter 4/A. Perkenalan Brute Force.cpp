4174b7737fe12cfadbf148b6b987e2c58429ebd5
Alfian Taka <pianaee379@gmail.com>
2026-05-08 13:58:41 +0700

    feat: `A. Perkenalan Brute Force` [WA]

---
+++ Books/Pemrograman Kompetitif Dasar/Chapter 4/A. Perkenalan Brute Force.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  string a, b; cin >> a >> b;
+  int miss = 0;
+
+  for (int i = 0; i < a.size(); i++) {
+    if (a[i] != b[i]) {
+      miss++;
+      a.erase(i, 1);
+    }
+  }
+
+  if (miss == 1) cout << "Tentu saja bisa!\n";
+  else cout << "Wah, tidak bisa :(\n";
+}
