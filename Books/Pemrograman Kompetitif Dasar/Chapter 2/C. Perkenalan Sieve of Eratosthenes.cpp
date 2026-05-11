58fa8108e823c77f9c22b95ae56bb59e4d241f92
Alfian Taka <pianaee379@gmail.com>
2026-05-11 17:06:55 +0700

    refactor: `C. Perkenalan Sieve of Eratosthenes`

--- Books/Pemrograman Kompetitif Dasar/Chapter 2/C. Perkenalan Sieve of Eratosthenes.cpp
+++ Books/Pemrograman Kompetitif Dasar/Chapter 2/C. Perkenalan Sieve of Eratosthenes.cpp
@@ -6,14 +6,14 @@ const int MAX = 1000001;
 
 int main() {
   int t; cin >> t;
-  bool isPrime[MAX];
-  for (int i = 0; i < MAX; i++) isPrime[i] = true;
+  vector<bool> isPrime(MAX, true);
 
   vector<int> primes;
   for (int i = 2; i < MAX; i++) {
     if (isPrime[i]) {
-      primes.push_back(i);
       int temp = i;
+      primes.push_back(i);
+
       while (temp < MAX) {
         temp = temp + i;
         if (temp < MAX) isPrime[temp] = false;
